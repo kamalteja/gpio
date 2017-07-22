cf2f586d6f7c0168d3460d54fce39cb316eeee7a
kamalteja <kamalteja.gurramkonda@gmail.com>
2017-07-22 11:16:29 +0000

    Added "time" definition

--- common/generic.py
+++ common/generic.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3.4
 
+#import
 import os
+import time
 
+#from
+from sys import exit
 
 class base:
     '''
@@ -11,6 +15,8 @@ class base:
     def __init__(self):
         self.tempThreshold = 40.0
         self.plinus = 10.0  # plus or minus tempThreshold
+        self.sup_formats = ["time", "localtime", "asctime"]
+        self.format = self.sup_formats[0]
 
     def getTempThreshold(self):
         return self.tempThreshold
@@ -24,3 +30,27 @@ class base:
         '''
         self.res = os.popen('vcgencmd measure_temp').readline()
         return(float(self.res.replace("temp=", "").replace("'C\n", "")))
+
+    def currentTime(self, format=None):
+        '''
+        Returns current time in asc format
+        Supported formats: time, localtime, asctime
+        '''
+        if format is None:
+            format = self.format
+        try:
+            assert type(format) is str and (format in self.sup_formats)
+            return str(getattr(time, format)())
+        except Exception as e:
+            print (str(e) + self.timeUsage())
+            exit(0)
+
+    def timeUsage(self):
+        '''
+        Prints the usage information to use currentTIme function
+        '''
+        usage = ""
+        usage += "Supported formats: "
+        for item in self.sup_formats:
+            usage += item + ", "
+        return usage
--- fancontrol/fanController.py
+++ fancontrol/fanController.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3.4
 
-from time import sleep
+#from
+from time import time, sleep, localtime, asctime
 from sys import exit
 from random import randint
 
@@ -29,25 +30,27 @@ def main():
         # Fancontroll loop
         while(True):
             cpuTemp = gl.getCPUtemperature()
+            time = gl.currentTime("asctime")
             if (cpuTemp > tempThreshold):
                 if not(gpio7.getGPIOoutput()):
                     gpio7.setGPIOoutput(True)
                 if not(gpio11.getGPIOoutput()):
                     gpio11.setGPIOoutput(True)
                 sleepTime = sleepTimeFanOn
-                print('CPU temperature crossed threshold: %s' % str(cpuTemp))
+                print('%s: CPU temperature crossed threshold: %s'
+                    %(time, str(cpuTemp)))
             elif (cpuTemp < tempThreshold):
                 if gpio7.getGPIOoutput():
                     gpio7.setGPIOoutput(False)
                 if gpio11.getGPIOoutput():
                     gpio11.setGPIOoutput(False)
                 sleepTime = sleepTimeFanOff
-                print('CPU temperature in limit: %s' % str(cpuTemp))
-            print('Rechecking in %d seconds' % sleepTime)
+                print('%s: CPU temperature in limit: %s' % (time, str(cpuTemp)))
+            print('%s: Rechecking in %d seconds' % (time, sleepTime))
             sleep(sleepTime)
     except (KeyboardInterrupt, Exception) as e:
         if e:
-            print('UnknownException: %s' % str(e))
+            print('%s: UnknownException: %s' % (time, str(e)))
         gpio7.resetGPIOPin()
         gpio11.resetGPIOPin()
         gpio7.cleanupGPIO()  # Can use any of the GPIOpin object to call
